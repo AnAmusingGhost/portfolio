98cd524e547ebe69f6f9d4782a64e9aebe70a61f
AnAmusingGhost <cflore63@depaul.edu>
2024-06-20 13:23:22 -0500

    added assests to the project
    added css to sidebar
    added content to sidebar.jsx

--- src/components/sidebar/sideBar.jsx
+++ src/components/sidebar/sideBar.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
 import "./sidebar.css"; 
+import Logo from '../../assets/logo.svg'
 
 const SideBar = () => {
 
     return(
+        <aside className="aside">
+            <a href="#home" className="nav__logo">
+                <img src={Logo} alt=""/>
+            </a>
 
-<di> sidebar</di>
+            <nav className="nav">
+                <div className="nav__menu">
+                    <ul className="nav__list">
+                       <li className="nav__item">
+                        <a href="#home" className="nav__link">
+                        <i class="icon-home"></i>
+                        </a>
+                        </li> 
 
+                        <li className="nav__item">
+                        <a href="#about" className="nav__link">
+                        <i class="icon-user-following"></i>
+                        </a>
+                        </li> 
+
+                        <li className="nav__item">
+                        <a href="#resume" className="nav__link">
+                        <i class="icon-graduation"></i>
+                        </a>
+                        </li> 
+
+                        <li className="nav__item">
+                        <a href="#portfolio" className="nav__link">
+                        <i class="icon-layers"></i>
+                        </a>
+                        </li> 
+                    </ul>
+                </div>
+            </nav>
+
+            <div className="nav__footer">
+                <span className="copyright"> 
+                    &copy; 2024
+                </span>
+            </div>
+        </aside>
     )
 }
 
