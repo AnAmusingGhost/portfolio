7831fc37a38d1d5a9fe978dff0b4f4a40eba2c32
AnAmusingGhost <cflore63@depaul.edu>
2024-06-20 14:29:49 -0500

    added extra css to home.css and home.css

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import "./home.css"; 
-
+import Me from "../../assets/avatar-1.svg"
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 const Home = () => {
 
     return(
 
-<di> Home</di>
+        <section className="home container">
+            <div className="intro">
+                <img src={Me} alt="" className="home__img" />
+                <h1 className="home__name">Carlos Flores</h1>
+                <span className="home__education">I'm an IT major with a UX minor.</span>
+                
+                <HeaderSocials />
+            
+                <a href="#contact" className="btn"> Contact Me!</a>
 
-    )
-}
+                <ScrollDown />
+            </div>
+        </section>
+    );
+};
 
 export default Home;
---
+++ src/components/home/ScrollDown.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const ScrollDown = () => {
+return(
+    <div className="scroll__down">
+        <a href="#about" className="mousew__wrapper">
+            <span className="home__scroll-name"> Scroll Down
+                <span className="mouse">
+                    <div className="wheel"></div>
+                </span>
+            </span>
+        </a>
+
+    </div>
+
+
+)
+
+
+}
+
+export default ScrollDown
